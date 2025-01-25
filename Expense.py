7e1dd7f40a6f85316ae7e9371ed5a25019f02ab2
Davyjones03 <159043538+Davyjones03@users.noreply.github.com>
2025-01-25 11:11:10 +0530

    Update Expense.py

--- Expense.py
+++ Expense.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="root",
+        password="your_password",
         database="expense_tracker"
     )
 
